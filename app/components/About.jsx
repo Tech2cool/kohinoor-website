d6c197b988e99390064d03174937421aba7422e6
shruti misal <shrutimisal109@gmail.com>
2025-06-06 13:25:45 +0530

    about in points

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -28,6 +28,34 @@ const About = () => {
         of <strong className={styles.impText}>13 committee members</strong>,
         ensuring smooth operations and the well-being of all residents.
       </div>
+      <div className={styles.aboutParaMobile}>
+        <ul>
+          <li>
+            <strong className={styles.impText}>KOHINOOR</strong> Co-operative
+            Housing Society (CHS) is located in Sector 8, Sanpada, Navi Mumbai,
+            Maharashtra 400705.
+          </li>
+          <li>
+            The society was established with the completion of its building
+            construction in{" "}
+            <strong className={styles.impText}>March 2001</strong>.
+          </li>
+          <li>
+            It comprises a total of{" "}
+            <strong className={styles.impText}>9 buildings</strong>.
+          </li>
+          <li>
+            Each building includes{" "}
+            <strong className={styles.impText}>16 residential units</strong>.
+          </li>
+          <li>
+            The society is managed by{" "}
+            <strong className={styles.impText}>13 committee members</strong> who
+            oversee daily operations and resident welfare.
+          </li>
+        </ul>
+      </div>
+
       <div className={styles.partitionDes}>
         <img src="/images/partition.png" alt="image" />
       </div>
